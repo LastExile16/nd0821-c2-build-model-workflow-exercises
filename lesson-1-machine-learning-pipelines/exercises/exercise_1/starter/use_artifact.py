a73bad9548d479a844cfa96429fa85d19a22af80
Nawras Nazar <nawras.nzar@gmail.com>
2024-09-11 16:29:50 +0900

    solution to the exercise

--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
@@ -19,6 +19,8 @@ def go(args):
     # YOUR CODE HERE: get the artifact and store its local path in the variable "artifact_path"
     # HINT: you can get the artifact path by using the "file()" method
 
+    logger.info("Getting artifact")
+    artifact = run.use_artifact("exercise_1/zen_of_python:v1")
     artifact_path = artifact.file()
 
     logger.info("Artifact content:")
